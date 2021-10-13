77cca26a1f64804e92d8c94f51c4954bc13ce02e
IRON-07 <88138150+IRON-07@users.noreply.github.com>
2021-10-13 13:51:43 +0700

    Add files via upload

---
+++ modules/commands/ad.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hà Mạc Trường Giang",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME : Nguyễn An Bình\nBiệt Danh: Mon ~~\nGiới Thiệu: Nhà anh không có gì ngoài nợ ngân hàng 10 tỉ\nI Have Something To You\nI Love You\nTính Cách : Cái gì cái chứ trêu gái là nhanh nhất nhé OwO\nChiều cao : 1m70\nSinh ngày : 23/12/2007\nLiên hệ:0867712627 \nInstagram: #####\nTikTok: #@!#@#!@#@!\nSở Thích: Thích chơi game, trêu gái\nCân nặng: 52kg\nUID FACEBOOK : 100073163596160\nName ID : ######### \nLink Facebook : https://www.facebook.com/profile.php?id=100073163596160\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n=== Nguyen Manh Khoa ( Cre hoang hai long ) ===`, event.threadID, event.messageID);
