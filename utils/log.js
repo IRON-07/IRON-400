d521b53b2fd64d0c8671d80ebaa7a57e249bc924
IRON-07 <88138150+IRON-07@users.noreply.github.com>
2021-10-15 16:00:32 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ J-JRT ] » ') + data);
+			console.log(chalk.yellow('[ I-IRON ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ J-JRT ] » ') + data);
+			console.log(chalk.red('[ I-IRON ] » ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[ J-JRT ] » `) + data);
+			console.log(chalk.green(`[ I-IRON ] » `) + data);
 			break;
 	}
 }
